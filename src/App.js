bd03aa29adddf133804350875e39a9e0224d02d2
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-11-08 13:55:29 -0500

    added article teext area

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ function App() {
             <h1>Blog with CSS Grid</h1>
           </header>
           <figure class="featured-image-1">
-            <img src={require('./components/images/6.jpeg')} />
+            <img src={require('./components/images/7.jpeg')} />
           </figure>
+          <div class="number-1">1</div>
+          <article class="article article-1"></article>
+          <h2>Blog with CSS Grid</h2>
+          <p>lorem ipsum</p>
         </div>
       </body>
     </div>
