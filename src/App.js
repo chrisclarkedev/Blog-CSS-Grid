bde73c24bd7bb6d1d0067f72c245fb017617af86
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-11-08 13:25:13 -0500

    first photo added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
           <header class="header">
             <h1>Blog with CSS Grid</h1>
           </header>
+          <figure class="featured-image-1">
+            <img src="https://images.unsplash.com/photo-1550792220-553fd02a239c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></img>
+          </figure>
         </div>
       </body>
     </div>
