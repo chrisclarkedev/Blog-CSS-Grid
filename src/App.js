107e6595e60be64501870f480f851f16c71724d4
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-11-09 16:35:50 -0500

    added 2nd image and article

--- src/App.js
+++ src/App.js
@@ -14,9 +14,18 @@ function App() {
             <img src={require('./components/images/7.jpeg')} />
           </figure>
           <div class="number-1">1</div>
-          <article class="article article-1"></article>
-          <h2>Blog with CSS Grid</h2>
-          <p>lorem ipsum</p>
+          <article class="article article-1">
+            <h2>Blog with CSS Grid</h2>
+            <p>lorem ipsum</p>
+          </article>
+          <figure class="featured-image-2">
+            <img src={require('./components/images/3.jpeg')} />
+          </figure>
+          <div class="number-2">2</div>
+          <article class="article article-2">
+            <h2>Blog with CSS Grid</h2>
+            <p>lorem ipsum</p>
+          </article>
         </div>
       </body>
     </div>
