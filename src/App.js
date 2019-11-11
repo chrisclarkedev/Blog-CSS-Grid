5c14ab93951d0463fb24eb7d1ce8cc564b915d7e
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-11-11 11:30:31 -0500

    added article to 2

--- src/App.js
+++ src/App.js
@@ -15,16 +15,16 @@ function App() {
           </figure>
           <div class="number-1">1</div>
           <article class="article article-1">
-            <h1>Marc Johnson</h1>
-            <p>All skateboarding is, is putting ideas into action</p>
+            <h1>Tony Hawk</h1>
+            <p>I won’t quit skating until I am physically unable.</p>
           </article>
           <figure class="featured-image-2">
             <img src={require('./components/images/3.jpeg')} />
           </figure>
           <div class="number-2">2</div>
           <article class="article article-2">
-            <h2>Blog with CSS Grid</h2>
-            <p>lorem ipsum</p>
+            <h2>Marc Johnson</h2>
+            <p>All skateboarding is, is putting ideas into action.</p>
           </article>
           <figure class="featured-image-3">
             <img src={require('./components/images/1.jpeg')} />
