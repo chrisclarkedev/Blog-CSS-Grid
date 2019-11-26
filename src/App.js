b5393741dca053f1a3a90d4f45f2fbda5972df32
chrisclarkedev <ceecee@CeeCees-MacBook-Air.local>
2019-11-26 10:41:09 -0500

    delted a post and fixed positioning

--- src/App.js
+++ src/App.js
@@ -43,14 +43,7 @@ function App() {
             <p>Skateboarding is the sound of living.</p>
           </article>
           {/* 5 & 6 */}
-          <figure class="featured-image-4">
-            <img src={require('./components/images/2.jpeg')} />
-          </figure>
-          <div class="number-5">4</div>
-          <article class="article article-5">
-            <h2>Rob Dyrdek</h2>
-            <p>In skateboarding your never bigger than the streets</p>
-          </article>
+
         </div>
       </body>
     </div>
